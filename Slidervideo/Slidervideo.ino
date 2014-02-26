466525ab1a49747066d4aeba24d0fc73b5b67f1b
Xav83130 <xgesset@free.fr>
2014-02-26 21:42:12 +0100

    Update Slidervideo.ino

--- Slidervideo/Slidervideo.ino
+++ Slidervideo/Slidervideo.ino
@@ -37,7 +37,7 @@ void setup()
 {
     Wire.begin(); // join i2c
   
-    Moteur1.init(750,200,1,MOTOR_MODE_HALFSTEP); // moteur 200 pas/tour au demi pas
+    Moteur1.init(750,200,2,MOTOR_MODE_HALFSTEP); // moteur 200 pas/tour au demi pas
     
     lcd.init(); // LCD init
     lcd.clear(); // Clear Display
